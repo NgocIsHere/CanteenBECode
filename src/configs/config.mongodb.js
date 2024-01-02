948588098d163a223574e908cc761cb3525b415c
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2024-01-03 05:03:08 +0700

    nothing

--- src/configs/config.mongodb.js
+++ src/configs/config.mongodb.js
@@ -3,9 +3,9 @@ const dev = {
     port: process.env.DEV_APP_PORT || 8080,
   },
   db: {
-    host: process.env.DEV_DB_HOST || "127.0.0.1",
-    port: process.env.DEV_DB_PORT || 27017,
-    name: process.env.DEV_DB_NAME || "canteenDEV",
+    // host: process.env.DEV_DB_HOST || "127.0.0.1",
+    // port: process.env.DEV_DB_PORT || 27017,
+    // name: process.env.DEV_DB_NAME || "canteenDEV",
     user: process.env.MONGO_USER || "borua1611",
     password: process.env.MONGO_PASSWORD || "ngoc123",
     dbname: process.env.MONGO_DB_NAME || "canteenmanagement"
