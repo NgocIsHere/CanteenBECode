bde3b482c5a308f76ef41bd484f61635b74a4d74
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2024-01-05 17:06:10 +0700

    apiv7

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -71,8 +71,9 @@ class ReportService {
         return await findInvReportByTime(Time);
     }
     static async getDInvReportDetail({reportId}) {
-        return await dInventoryReport.findOne({_id: reportId});
-    //   .populate("inventory_list");
+        const invR = await dInventoryReport.findOne({_id: reportId});
+        const idlist = invR.inventory_list;
+        return await invReportItem.find({ _id: { $in: idlist } });
     }
     static async createDIncReport(userId) {
         //user_id,sale_quantity,loss_quantity,profit,loss_money,
