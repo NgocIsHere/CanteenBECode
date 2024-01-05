06f1eb3fb8f05c94e125b38d94b43050cff0c61f
NgocIsHere <111295106+NgocIsHere@users.noreply.github.com>
2024-01-05 07:09:55 +0700

    api

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -3,7 +3,7 @@ import config from "../configs/config.mongodb.js";
 import { countConnect } from "../helpers/check.connect.js";
 const env = process.env.NODE_ENV || "dev";
 const { db } = config[env] || {};  
-const { host, port, name } = db || {};
+const { host, port, name } = db;
 const connects= {
   dev: `mongodb://${host}:${port}/${name}`,
   pro: `mongodb+srv://${host}:${port}@canteenmanagement.qbhpovo.mongodb.net/${name}?retryWrites=true&w=majority`,
